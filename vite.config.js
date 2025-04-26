805258464ddc7e938b7efafd084dd162553dc6fa
Adrien1111131 <adrien.cazanave@gmail.com>
2025-04-27 01:37:25 +0200

    Update repository name and rebuild

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/bottradbct/',
+  base: '/botTRAD-B.T.C/',
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
